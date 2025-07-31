421ecea3094c1ed24689be41ecf217743d67513c
Brian <brianizzo93@gmail.com>
2025-07-31 22:20:38 +0900

    added display_name to be returned

--- src/utils/userlib.ts
+++ src/utils/userlib.ts
@@ -4,7 +4,7 @@ import pool from "../utils/db";
 
 export async function tokenUser(decodedToken: any) {
 	return await pool.query(
-   "SELECT id, role FROM users WHERE id = $1",[decodedToken.id]
+   "SELECT id, role, display_name FROM users WHERE id = $1",[decodedToken.id]
 	);
   }
 
