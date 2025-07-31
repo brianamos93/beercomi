bd914a7e83de591641f4a4bc84b10a7aebcf7403
Brian <brianizzo93@gmail.com>
2025-07-31 22:21:07 +0900

    added getTokenFrom import and authenticationHandler export

--- src/utils/middleware.js
+++ src/utils/middleware.js
@@ -1,5 +1,6 @@
 const logger = require('./logger')
 const jwt = require('jsonwebtoken')
+const { getTokenFrom } = require('./userlib');
 
 const requestLogger = (request, response, next) => {
   logger.info('Method:', request.method)
@@ -42,5 +43,6 @@ const authenticationHandler = (req, res, next) => {
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  authenticationHandler
 }
