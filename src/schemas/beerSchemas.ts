35592cdb41a3c7d4727b8e7d7024caf234ce516f
Brian <brianizzo93@gmail.com>
2025-10-17 23:17:10 +0900

    removed unnessary variables and reformated code

--- src/schemas/beerSchemas.ts
+++ src/schemas/beerSchemas.ts
@@ -1,47 +1,36 @@
-import { z } from "zod"
+import { z } from "zod";
 
-const MAX_FILE_SIZE = 1000000;
-const ACCEPTED_IMAGE_TYPES = [
-  'image/jpeg',
-  'image/jpg',
-  'image/png',
-  'image/webp'
-];
-
-const multerFileSchema = z.object({
-  fieldname: z.string(),
-  originalname: z.string(),
-  encoding: z.string(),
-  mimetype: z.string(),
-  size: z.number(),
-});
+const multerFileSchema = z
+	.object({
+		fieldname: z.string(),
+		originalname: z.string(),
+		encoding: z.string(),
+		mimetype: z.string(),
+		size: z.number(),
+		buffer: z.instanceof(Buffer),
+	})
+	.refine(
+		(file) => ["image/jpeg", "image/png", "image/webp"].includes(file.mimetype),
+		{ message: "Only JPEG, PNG, or WEBP images are allowed." }
+	)
+	// Limit file size to 1MB
+	.refine((file) => file.size <= 1_000_000, {
+		message: "File must be less than 1MB.",
+	});
 
 export const BeerSchemaBase = z.object({
-	name: z
-		.string()
-		.trim()
-		.min(1, "Name is required."),
-	style: z
-		.string()
-		.trim()
-		.min(1, "Style is required."),
+	name: z.string().trim().min(1, "Name is required."),
+	style: z.string().trim().min(1, "Style is required."),
 	abv: z.coerce.number().min(0, "ABV must be a positive number."),
-	brewery_id: z
-		.string()
-		.min(1, "A brewery is required."),
-	color: z
-		.string()
-		.min(1, "Color is required."),
+	brewery_id: z.string().min(1, "A brewery is required."),
+	color: z.string().min(1, "Color is required."),
 	ibu: z.coerce.number().min(0, "IBU must be a positive number."),
-	description: z
-		.string()
-		.min(1, "Description is required."),
-	cover_image: multerFileSchema
-		
-})
+	description: z.string().min(1, "Description is required."),
+	cover_image: multerFileSchema,
+});
 
 export const EditBeerSchema = BeerSchemaBase.extend({
-	deleteCoverImage: z.boolean()
-})
+	deleteCoverImage: z.boolean(),
+});
 
 export type EditBeerInput = z.infer<typeof EditBeerSchema>;
