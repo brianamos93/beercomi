e8e31731c07515fe13a67338147199dea616d4b9
Brian <brianizzo93@gmail.com>
2025-11-01 11:47:57 +0900

    added zod validation to brewery post and put

--- src/routes/breweryRoutes.ts
+++ src/routes/breweryRoutes.ts
@@ -2,24 +2,25 @@ import { Router, Request, Response } from "express";
 import pool from "../utils/config";
 import { decodeToken } from "../utils/userlib";
 import multer from "multer";
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 import { FileFilterCallback } from "multer";
 import sharp from "sharp";
 import { userIdGet } from "./userRoutes";
+import { validationHandler } from "../utils/validationMiddleware";
+import {
+	BrewerySchemaBase,
+	EditBrewerySchema,
+} from "../schemas/brewerySchemas";
 const { authenticationHandler } = require("../utils/middleware");
-const express = require('express')
-
-
+const express = require("express");
 
 const router = Router();
 
-
-
-declare module 'express-serve-static-core' {
-  interface Request {
-	user?: { id: string; role: string };
-  }
+declare module "express-serve-static-core" {
+	interface Request {
+		user?: { id: string; role: string };
+	}
 }
 
 interface Brewery {
@@ -33,59 +34,75 @@ interface Brewery {
 	owner: string;
 }
 
-const fileFilter = (_req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
-  // Reject empty files (size 0 or name 'undefined')
-  if (!file.originalname || file.size === 0 || file.originalname === 'undefined') {
-	// Skip this file, treat as no file uploaded
-	return cb(null, false);
-  }
-  const allowedTypes = ['image/jpeg', 'image/png'];
-  if (allowedTypes.includes(file.mimetype)) cb(null, true);
-  else cb(new Error('Only .jpeg and .png files are allowed'));
+const fileFilter = (
+	_req: Request,
+	file: Express.Multer.File,
+	cb: FileFilterCallback
+) => {
+	// Reject empty files (size 0 or name 'undefined')
+	if (
+		!file.originalname ||
+		file.size === 0 ||
+		file.originalname === "undefined"
+	) {
+		// Skip this file, treat as no file uploaded
+		return cb(null, false);
+	}
+	const allowedTypes = ["image/jpeg", "image/png"];
+	if (allowedTypes.includes(file.mimetype)) cb(null, true);
+	else cb(new Error("Only .jpeg and .png files are allowed"));
 };
 
 const upload = multer({ storage: multer.memoryStorage(), fileFilter });
 
-
 async function brewerylookup(breweryID: string) {
-	return await pool.query("SELECT id, name, location, date_of_founding, author_id FROM breweries WHERE id = $1", [breweryID]);
-  }
+	return await pool.query(
+		"SELECT id, name, location, date_of_founding, author_id FROM breweries WHERE id = $1",
+		[breweryID]
+	);
+}
 
 async function breweryUser(breweryID: string) {
-	return await pool.query("SELECT author_id FROM breweries WHERE id = $1", [breweryID]);
-  }
+	return await pool.query("SELECT author_id FROM breweries WHERE id = $1", [
+		breweryID,
+	]);
+}
 
 async function breweryCoverImageLookup(breweryID: string) {
-	return await pool.query("SELECT cover_image FROM breweries WHERE id = $1", [breweryID]);
-  }
-
+	return await pool.query("SELECT cover_image FROM breweries WHERE id = $1", [
+		breweryID,
+	]);
+}
 
 router.get("/", express.json(), async (req: Request, res: Response) => {
 	try {
-		const result = await pool.query("SELECT * FROM breweries ORDER BY date_updated DESC");
+		const result = await pool.query(
+			"SELECT * FROM breweries ORDER BY date_updated DESC"
+		);
 		const breweries: Brewery[] = result.rows;
 		res.json(breweries);
-	  } catch (error) {
+	} catch (error) {
 		console.error("Error fetching breweries", error);
 		res.status(500).json({ error: "Error fetching breweries" });
-	  }
-	});
+	}
+});
 
 router.get("/list", express.json(), async (req: Request, res: Response) => {
 	try {
 		const result = await pool.query("SELECT id FROM breweries");
 		const breweries: Brewery[] = result.rows;
 		res.json(breweries);
-		} catch (error) {
+	} catch (error) {
 		console.error("Error fetching breweries", error);
 		res.status(500).json({ error: "Error fetching breweries" });
-		}
-	});	
+	}
+});
 
 router.get("/:id", express.json(), async (req: Request, res: Response) => {
-	const breweryId = req.params.id
+	const breweryId = req.params.id;
 	try {
-		const result = await pool.query(`SELECT 
+		const result = await pool.query(
+			`SELECT 
 	breweries.id, 
 	breweries.name, 
 	breweries.location, 
@@ -114,151 +131,200 @@ LEFT JOIN users AS brewery_authors ON breweries.author_id = brewery_authors.id
 LEFT JOIN beers ON breweries.id = beers.brewery_id 
 LEFT JOIN users AS beer_authors ON beers.author_id = beer_authors.id
 WHERE breweries.id = $1 
-GROUP BY breweries.id, brewery_authors.display_name;`, [breweryId])
+GROUP BY breweries.id, brewery_authors.display_name;`,
+			[breweryId]
+		);
 		const breweries: Brewery[] = result.rows[0];
-		res.json(breweries)
+		res.json(breweries);
 	} catch (error) {
-		console.error("Error fetching breweries", error)
-		res.status(500).json({ error: "Error fetching breweries" })
+		console.error("Error fetching breweries", error);
+		res.status(500).json({ error: "Error fetching breweries" });
 	}
-})	
+});
 
-router.post("/", authenticationHandler, upload.single('cover_image'), async (req: Request, res: Response) => {
-	const { name, location, date_of_founding } = req.body;
+router.post(
+	"/",
+	authenticationHandler,
+	upload.single("cover_image"),
+	validationHandler(BrewerySchemaBase),
+	async (req: Request, res: Response) => {
+		const { name, location, date_of_founding } = req.body;
 
-	let newFileName = null
-	let relativeUploadFilePathAndFile = null
+		let newFileName = null;
+		let relativeUploadFilePathAndFile = null;
 
-	if (!req.user || !req.user.id) {
-	return res.status(401).json({ error: "Unauthorized: user not found" });
-	}
-	const user = await pool.query(
-	"SELECT * FROM users WHERE id = $1", [req.user.id]
-	);
- 
-	// TypeScript type-based input validation
-	if (typeof name !== "string" || name.trim() === "") {
-	  return res.status(400).json({ error: "Invalid brewery name data" });
-	}
-	if (typeof location !== "string" || location.trim() === "") {
-	  return res.status(400).json({ error: "Invalid brewery location data" });
-	}
-	if (typeof date_of_founding !== "string" || date_of_founding.trim() === "") {
-	  return res.status(400).json({ error: "Invalid date_of_founding data" });
-	}
-	if (req.file) {
-	
-		const uploadPath = path.join(__dirname, '..', `uploads/${name}`);
-		if (!fs.existsSync(uploadPath)) {
-			fs.mkdirSync(uploadPath, { recursive: true });
+		if (!req.user || !req.user.id) {
+			return res.status(401).json({ error: "Unauthorized: user not found" });
 		}
-		const ext: string = req.file && req.file.originalname ? path.extname(req.file.originalname) : '';
-		newFileName = `${name}CoverImage-${Date.now()}${ext}`
-		const uploadFilePathAndFile = path.join(uploadPath, newFileName)
-		await sharp(req.file.buffer).resize(200,200).toFile(uploadFilePathAndFile)
-		relativeUploadFilePathAndFile = `/uploads/${name}/${newFileName}`
-		}
-	try {
-	  const result = await pool.query(
-		"INSERT INTO breweries (name, location, date_of_founding, author_id, cover_image) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-		[name, location, date_of_founding, user.rows[0].id, relativeUploadFilePathAndFile]
-	  );
-	  const createdBrewery: Brewery = result.rows[0];
-	  res.status(201).json(createdBrewery);
-	} catch (error) {
-	  console.error("Error adding brewery", error);
-	  res.status(500).json({ error: "Error adding brewery" });
-	}
-  });
- 
- router.put("/:id", authenticationHandler, upload.single('cover_image'), async (req: Request, res: Response) => {
-	const breweryID = req.params.id
-	const { name, location, date_of_founding } = req.body;
-
-	const brewerycheck = await brewerylookup(breweryID)
- 
-	if (brewerycheck.rowCount == 0) {
-	 return res.status(401).json({ error: 'brewery does not exist'})
-	}
- 
-	const decodedToken = decodeToken(req)
-	if (!decodedToken.id) {
-	 return res.status(401).json({ error: 'token invalid'})
-	}
-  
-	const breweryuser = await breweryUser(breweryID)
-
-	if (!req.user || !req.user.id) {
-		return res.status(401).json({ error: "Unauthorized: user not found" });
-	}
-	const userData = await userIdGet(req.user.id)
-	const userRole = userData.rows[0].role
- 
-	if (req.user.id !== breweryuser.rows[0].author_id && userRole !== 'admin') {
-	 return res.status(400).json({ error: "User not authorized" })
-	}
-	let relativeUploadFilePathAndFile
-
-	if (req.file) {
-	
-		const uploadPath = path.join(__dirname, '..', `uploads/${name}`);
-		if (!fs.existsSync(uploadPath)) {
-			fs.mkdirSync(uploadPath);
-		}
-		const ext: string = req.file && req.file.originalname ? path.extname(req.file.originalname) : '';
-		const newFileName = `${name}-CoverImage-${Date.now()}${ext}`
-		const uploadFilePathAndFile = path.join(uploadPath, newFileName)
-		await sharp(req.file.buffer).resize(200,200).toFile(uploadFilePathAndFile)
-		relativeUploadFilePathAndFile = `/uploads/${name}/${newFileName}`;
-	}
- 
-	try {
-	  await pool.query("UPDATE breweries SET name = $1, location = $2, date_of_founding = $3, cover_image = $4 WHERE id = $5", [name, location, date_of_founding, relativeUploadFilePathAndFile, breweryID]);
-		res.status(200).json({ message: "Brewery updated successfully" });
-	} catch (error) {
-	  console.error("PUT /:id error:", error);
-	  res.status(500).json({ error: "Error" });
-	}
- 
- 
- }); 
-
-router.delete("/:id", express.json(), authenticationHandler, async (req: Request, res: Response) => {
-	const breweryID = req.params.id
-	const brewerycheck = await brewerylookup(breweryID)
-	if (brewerycheck.rowCount == 0) {
-		return res.status(401).json({ error: 'brewery does not exist'})
-	}
-	const decodedToken = decodeToken(req)
-	if (!decodedToken.id) {
-		return res.status(401).json({ error: 'token invalid'})
-	}
-	if (!req.user || !req.user.id) {
-		return res.status(401).json({ error: "Unauthorized: user not found" });
-	}
-	const breweryUserResult = await breweryUser(breweryID)
-	const userData = await userIdGet(req.user.id)
-	const userRole = userData.rows[0].role
-
-	if (req.user.id !== breweryUserResult.rows[0].authorid && userRole !== 'admin') {
-		return res.status(400).json({ error: "User not authorized" })
-	}
-
-	const breweryCoverImageRes = await breweryCoverImageLookup(breweryID)
-		const coverImagePathAndFile = breweryCoverImageRes.rows[0].cover_image
-		if(fs.existsSync(coverImagePathAndFile)) {
-			fs.unlinkSync(coverImagePathAndFile)
+		const user = await pool.query("SELECT * FROM users WHERE id = $1", [
+			req.user.id,
+		]);
+		//Process uploaded file
+		if (req.file) {
+			const uploadPath = path.join(__dirname, "..", `uploads/${name}`);
+			if (!fs.existsSync(uploadPath)) {
+				fs.mkdirSync(uploadPath, { recursive: true });
 			}
-
-	try {
-	  await pool.query("DELETE FROM breweries WHERE id = $1", [breweryID]);
-	  res.sendStatus(200);
-	} catch (error) {
-	  console.error("Error deleting brewery", error);
-	  res.status(500).json({ error: "Error deleting brewery" });
+			const ext: string =
+				req.file && req.file.originalname
+					? path.extname(req.file.originalname)
+					: "";
+			newFileName = `${name}CoverImage-${Date.now()}${ext}`;
+			const uploadFilePathAndFile = path.join(uploadPath, newFileName);
+			await sharp(req.file.buffer)
+				.resize(200, 200)
+				.toFile(uploadFilePathAndFile);
+			relativeUploadFilePathAndFile = `/uploads/${name}/${newFileName}`;
+		}
+		try {
+			const result = await pool.query(
+				"INSERT INTO breweries (name, location, date_of_founding, author_id, cover_image) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+				[
+					name,
+					location,
+					date_of_founding,
+					user.rows[0].id,
+					relativeUploadFilePathAndFile,
+				]
+			);
+			const createdBrewery: Brewery = result.rows[0];
+			res.status(201).json(createdBrewery);
+		} catch (error) {
+			console.error("Error adding brewery", error);
+			res.status(500).json({ error: "Error adding brewery" });
+		}
 	}
-  }); 
+);
 
-  
+router.put(
+	"/:id",
+	authenticationHandler,
+	upload.single("cover_image"),
+	validationHandler(EditBrewerySchema),
+	async (req: Request, res: Response) => {
+		const breweryID = req.params.id;
+		const { name, location, date_of_founding, deleteCoverImage } = req.body;
+
+		const brewerycheck = await brewerylookup(breweryID);
+
+		if (brewerycheck.rowCount == 0) {
+			return res.status(401).json({ error: "brewery does not exist" });
+		}
+
+		const decodedToken = decodeToken(req);
+		if (!decodedToken.id) {
+			return res.status(401).json({ error: "token invalid" });
+		}
+
+		const breweryuser = await breweryUser(breweryID);
+
+		if (!req.user || !req.user.id) {
+			return res.status(401).json({ error: "Unauthorized: user not found" });
+		}
+		const userData = await userIdGet(req.user.id);
+		const userRole = userData.rows[0].role;
+
+		if (req.user.id !== breweryuser.rows[0].author_id && userRole !== "admin") {
+			return res.status(400).json({ error: "User not authorized" });
+		}
+		
+		if (deleteCoverImage === true) {
+			const currentCoverImage = brewerycheck.rows[0].cover_image;
+			if (!currentCoverImage) {
+				res.status(404).json({ error: "No cover image found." });
+			}
+			const filePath = path.join(__dirname, "..", currentCoverImage);
+			if (fs.existsSync(filePath)) {
+				fs.unlinkSync(filePath);
+			}
+			const result = await pool.query(
+				`UPDATE breweries SET cover_image = NULL WHERE id = $1`,
+				[breweryID]
+			);
+			if (result.rowCount === 0) {
+				console.log("No brewery found with that ID");
+			}
+		}
+		let relativeUploadFilePathAndFile;
+
+		if (req.file) {
+			const uploadPath = path.join(__dirname, "..", `uploads/${name}`);
+			if (!fs.existsSync(uploadPath)) {
+				fs.mkdirSync(uploadPath);
+			}
+			const ext: string =
+				req.file && req.file.originalname
+					? path.extname(req.file.originalname)
+					: "";
+			const newFileName = `${name}-CoverImage-${Date.now()}${ext}`;
+			const uploadFilePathAndFile = path.join(uploadPath, newFileName);
+			await sharp(req.file.buffer)
+				.resize(200, 200)
+				.toFile(uploadFilePathAndFile);
+			relativeUploadFilePathAndFile = `/uploads/${name}/${newFileName}`;
+		}
+
+		try {
+			await pool.query(
+				"UPDATE breweries SET name = $1, location = $2, date_of_founding = $3, cover_image = $4 WHERE id = $5",
+				[
+					name,
+					location,
+					date_of_founding,
+					relativeUploadFilePathAndFile,
+					breweryID,
+				]
+			);
+			res.status(200).json({ message: "Brewery updated successfully" });
+		} catch (error) {
+			console.error("PUT /:id error:", error);
+			res.status(500).json({ error: "Error" });
+		}
+	}
+);
+
+router.delete(
+	"/:id",
+	express.json(),
+	authenticationHandler,
+	async (req: Request, res: Response) => {
+		const breweryID = req.params.id;
+		const brewerycheck = await brewerylookup(breweryID);
+		if (brewerycheck.rowCount == 0) {
+			return res.status(401).json({ error: "brewery does not exist" });
+		}
+		const decodedToken = decodeToken(req);
+		if (!decodedToken.id) {
+			return res.status(401).json({ error: "token invalid" });
+		}
+		if (!req.user || !req.user.id) {
+			return res.status(401).json({ error: "Unauthorized: user not found" });
+		}
+		const breweryUserResult = await breweryUser(breweryID);
+		const userData = await userIdGet(req.user.id);
+		const userRole = userData.rows[0].role;
+
+		if (
+			req.user.id !== breweryUserResult.rows[0].authorid &&
+			userRole !== "admin"
+		) {
+			return res.status(400).json({ error: "User not authorized" });
+		}
+
+		const breweryCoverImageRes = await breweryCoverImageLookup(breweryID);
+		const coverImagePathAndFile = breweryCoverImageRes.rows[0].cover_image;
+		if (fs.existsSync(coverImagePathAndFile)) {
+			fs.unlinkSync(coverImagePathAndFile);
+		}
+
+		try {
+			await pool.query("DELETE FROM breweries WHERE id = $1", [breweryID]);
+			res.sendStatus(200);
+		} catch (error) {
+			console.error("Error deleting brewery", error);
+			res.status(500).json({ error: "Error deleting brewery" });
+		}
+	}
+);
 
 export default router;
