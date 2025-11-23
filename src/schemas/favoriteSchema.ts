101391d397d0c56f5cadbb81c6a1a4694717c478
Brian <brianizzo93@gmail.com>
2025-11-23 20:23:18 +0900

    added schema for favorite route

---
+++ src/schemas/favoriteSchema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod";
+
+export const favoriteInputSchema = z.object({
+	target_id: z.uuidv4(),
+	table: z.enum(["beers", "breweries"]),
+})
+
+export const favoriteDeleteSchema = z.object({
+	table: z.enum(["beers", "breweries"]),
+	id: z.uuidv4(),
+})
+
+export const favoriteGetTableSchema = z.object({
+	table: z.enum(["beers", "breweries"]),
+})
