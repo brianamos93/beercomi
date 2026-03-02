c95e227e83148f089a0e608ef25293321163eb7e
Brian <brianizzo93@gmail.com>
2026-03-02 21:37:55 +0900

    removed multer schema and added coerce to boolean

--- src/schemas/beerSchemas.ts
+++ src/schemas/beerSchemas.ts
@@ -1,23 +1,5 @@
 import { z } from "zod";
 
-const multerFileSchema = z
-	.object({
-		fieldname: z.string(),
-		originalname: z.string(),
-		encoding: z.string(),
-		mimetype: z.string(),
-		size: z.number(),
-		buffer: z.instanceof(Buffer),
-	})
-	.refine(
-		(file) => ["image/jpeg", "image/png", "image/webp"].includes(file.mimetype),
-		{ message: "Only JPEG, PNG, or WEBP images are allowed." }
-	)
-	// Limit file size to 1MB
-	.refine((file) => file.size <= 1_000_000, {
-		message: "File must be less than 1MB.",
-	});
-
 export const BeerSchemaBase = z.object({
 	name: z.string().trim().min(1, "Name is required."),
 	style: z.string().trim().min(1, "Style is required."),
@@ -29,7 +11,7 @@ export const BeerSchemaBase = z.object({
 });
 
 export const EditBeerSchema = BeerSchemaBase.extend({
-	deleteCoverImage: z.boolean(),
+	deleteCoverImage: z.coerce.boolean(),
 });
 
 export type EditBeerInput = z.infer<typeof EditBeerSchema>;
