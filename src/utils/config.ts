4277895896cd181d580acd627cd44c1db6020930
Brian <brianizzo93@gmail.com>
2026-03-12 18:30:00 +0900

    added error handler for db connection issues

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -11,4 +11,8 @@ const pool = new pg.Pool({
   connectionString: process.env.DB_URL,
 });
 
+pool.on("error", (err) => {
+  console.error("Unexpected database error", err);
+});
+
 export default pool;
