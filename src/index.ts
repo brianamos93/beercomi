8fe9fc609718ff2ab85f246ef7f22fc1dca382dc
Brian <brianizzo93@gmail.com>
2026-03-17 21:30:31 +0900

    changed port config

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 
 
-app.listen(3005, () =>
+app.listen(process.env.PORT, () =>
 	console.log('REST API server ready at: http://localhost:3005'),
   )
