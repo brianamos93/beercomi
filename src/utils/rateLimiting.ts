01ab9659e214b982c83bfefe1e3df6c5490c151a
Brian <brianizzo93@gmail.com>
2026-03-26 00:04:32 +0900

    removed key generator

--- src/utils/rateLimiting.ts
+++ src/utils/rateLimiting.ts
@@ -1,9 +1,9 @@
-import { ipKeyGenerator, rateLimit } from 'express-rate-limit'
+import { rateLimit } from 'express-rate-limit'
 
 export const limiter = rateLimit({
     windowMs: 1 * 60 * 1000,
     limit: 200,
     standardHeaders: true,
     legacyHeaders: false,
-    keyGenerator: (req) => ipKeyGenerator(req.ip ?? 'unknown'),
+    ipv6Subnet: 56,
 })
