26c0b46ef055bab52a4dc35e1e9e5065e3e3d6bd
Brian <brianizzo93@gmail.com>
2026-03-29 17:19:01 +0900

    added deleteCoverImageData

--- src/utils/lib/ImageUpload.ts
+++ src/utils/lib/ImageUpload.ts
@@ -2,12 +2,9 @@ import path from "path";
 import fs from "fs";
 import sharp from "sharp";
 import { MulterRequest } from "../../defs/general.defs";
+import { BreweryModel } from "../../models/brewery.model";
 
-export async function imageUpload({
-	req,
-}: {
-	req: MulterRequest;
-}) {
+export async function imageUpload({ req }: { req: MulterRequest }) {
 	const uploadPath = path.join(__dirname, "..", `uploads/`);
 	if (!fs.existsSync(uploadPath)) {
 		fs.mkdirSync(uploadPath, { recursive: true });
@@ -24,4 +21,22 @@ export async function imageUpload({
 		.toFile(uploadFilePathAndFile);
 	const relativeUploadFilePathAndFile = `/uploads/${newFileName}`;
 	return relativeUploadFilePathAndFile;
+}
+
+export async function deleteCoverImageData({
+	id,
+	coverImagePath,
+}: {
+	id: string;
+	coverImagePath: string;
+}) {
+	const filePath = path.join(__dirname, "..", coverImagePath);
+	if (fs.existsSync(filePath)) {
+		fs.unlinkSync(filePath);
+	}
+	const result = await BreweryModel.nullCoverImage(id)
+	if (result.rowCount === 0) {
+		throw new Error("NO_BREWERY")
+	}
+	return result
 }
